e029bae3fe01b58d5f9967100ab24eb3fc08990d
Sean <sinbad4u007@yahoo.com>
2024-01-04 06:51:16 -0800

    an empty class Rectangle that defines a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,5 +2,5 @@
 """Defines a Rectangle class."""
 
 class Rectangle:
-	"""Represent a rectangle."""
-	pass
+"""Represent a rectangle."""
+pass
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a readme file for the projec on python.
